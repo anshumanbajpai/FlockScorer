ea42986be39874817de5075941eb45a2a2d7528b
anshuman <anshumanbajpai123@gmail.com>
2016-02-11 21:39:10 +0530

    refactor code & add support for modifying score through integer

--- src/main/java/co/flock/controller/Controller.java
+++ src/main/java/co/flock/controller/Controller.java
@@ -32,24 +32,43 @@ public class Controller {
         String message = (String) req.get(FILED_TEXT);
         if (message != null) {
             message = message.trim();
-            if (isScorerCounterModificationMessage(message)) {
+            if (isScoreCheckerOrModifierMessage(message)) {
                 String msgWithPrefixRemoved = message.substring(message.indexOf('@') + 1).trim();
                 String[] splited = SPACE_PATTERN.split(msgWithPrefixRemoved);
                 if (splited.length > 1) {
                     String last = splited[splited.length - 1];
-                    if (last.compareTo("++") == 0) {
+                    boolean validInput;
+                    Integer score = 0;
+                    try {
+                        score = Integer.valueOf(last);
+                        validInput = true;
+                    } catch (NumberFormatException ex) {
+                        validInput = false;
+                    }
+
+                    if (validInput) {
                         String name = extractName(splited);
-                        responseMap.put(name, String.valueOf(modifyScore(name, true)));
+                        responseMap.put(name, String.valueOf(putScore(name, score)));
                         FlockMessagePoster.Post(getFormattedText(responseMap));
                         return responseMap;
-                    } else if (last.compareTo("--") == 0) {
+                    } else if (last.compareTo("++") == 0 || last.compareTo("--") == 0) {
+                        boolean increment = last.compareTo("++") == 0;
                         String name = extractName(splited);
-                        responseMap.put(name, String.valueOf(modifyScore(name, false)));
+                        responseMap.put(name, String.valueOf(modifyScore(name, increment)));
                         FlockMessagePoster.Post(getFormattedText(responseMap));
                         return responseMap;
+                    } else {
+                        String name = message.substring(message.indexOf('@') + 1).trim();
+                        if (getScoreMap().containsKey(name)) {
+                            responseMap.put(name, String.valueOf(getScoreMap().get(name)));
+                            FlockMessagePoster.Post(getFormattedText(responseMap));
+                            return responseMap;
+                        } else {
+                            FlockMessagePoster.Post("Not found!");
+                        }
                     }
                 }
-            } else if (isScorerLeaderBoardMessage(message)) {
+            } else if (isLeaderboardFetcherMessage(message)) {
                 Map<String, String> topScorers = getTopScorers(getScoreMap(), false, MAX_TOP_SCORERS);
                 if (!topScorers.isEmpty()) {
                     FlockMessagePoster.Post(getFormattedText(topScorers));
@@ -57,22 +76,13 @@ public class Controller {
                 } else {
                     FlockMessagePoster.Post("No scores!");
                 }
-            } else if (isScorerIndiScoreMessage(message)) {
-                String name = message.substring(message.indexOf('@') + 1).trim();
-                if (getScoreMap().containsKey(name)) {
-                    responseMap.put(name, String.valueOf(getScoreMap().get(name)));
-                    FlockMessagePoster.Post(getFormattedText(responseMap));
-                    return responseMap;
-                } else {
-                    FlockMessagePoster.Post("Not found!");
-                }
             }
         }
 
         return null;
     }
 
-    private static boolean isScorerIndiScoreMessage(String message) {
+    private static boolean isScoreCheckerOrModifierMessage(String message) {
         return StringUtils.containsIgnoreCase(message, "scorer") && message.contains("@");
     }
 
@@ -87,12 +97,8 @@ public class Controller {
         return response;
     }
 
-    private static boolean isScorerLeaderBoardMessage(String message) {
+    private static boolean isLeaderboardFetcherMessage(String message) {
         return StringUtils.containsIgnoreCase(message, "scorer") && StringUtils.containsIgnoreCase(message, "top");
-    }
-
-    private static boolean isScorerCounterModificationMessage(String message) {
-        return StringUtils.containsIgnoreCase(message, "scorer") && message.contains("@") && (message.contains("++") || message.contains("--"));
     }
 
     private int modifyScore(String name, boolean increment) {
@@ -119,6 +125,21 @@ public class Controller {
             scoreMap.put(name, score);
         }
 
+        return scoreMap.get(name);
+    }
+
+    private int putScore(String name, int score) {
+        Map<String, Integer> scoreMap = getScoreMap();
+        if (scoreMap.containsKey(name)) {
+            Integer oldScore = scoreMap.get(name);
+            int newScore = oldScore + score;
+            userRepository.delete(new User(name, 0));
+            userRepository.put(new User(name, newScore));
+            scoreMap.put(name, newScore);
+        } else {
+            userRepository.put(new User(name, score));
+            scoreMap.put(name, score);
+        }
         return scoreMap.get(name);
     }
 
