30de466dec2080f129b166e97dcd18bba030ede9
anshuman <anshumanbajpai123@gmail.com>
2016-02-12 11:37:47 +0530

    extract out incoming webhook url to properties file

--- src/main/java/co/flock/FlockMessagePoster.java
+++ src/main/java/co/flock/FlockMessagePoster.java
@@ -8,17 +8,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class FlockMessagePoster {
+
     private static final OkHttpClient client = new OkHttpClient();
-    private static final String FLOCK_INCOMING_WEBHOOK_URL = "https://api.flock.co/hooks/sendMessage/e46b89ae-bddc-4763-8877-5df3ef74d1ea";
+    private static String flockIncomingWebhookUrl;
     private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public static void Post(String text) {
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("text",text);
+        jsonObject.put("text", text);
         RequestBody body = RequestBody.create(JSON, jsonObject.toString());
         Request request = new Request.Builder()
-                .url(FLOCK_INCOMING_WEBHOOK_URL)
+                .url(flockIncomingWebhookUrl)
                 .post(body)
                 .build();
         executor.submit(new Runnable() {
@@ -31,5 +32,9 @@ public class FlockMessagePoster {
                 }
             }
         });
+    }
+
+    public static void setFlockIncomingWebhookUrl(String flockIncomingWebhookUrl) {
+        FlockMessagePoster.flockIncomingWebhookUrl = flockIncomingWebhookUrl;
     }
 }
